cbd8459f8fbfdd718a1d3b94cc00e4730cc9f86b
J0hnFe <johnfe@outlook.es>
2023-07-06 12:14:15 -0500

    Commit inicial

---
+++ src/main/java/com/example/demo/TareaQueriesApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaQueriesApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaQueriesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Commit inicial");
+	}
+
+}
